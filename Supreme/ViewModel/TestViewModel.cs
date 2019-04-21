91d9b90c54e878728f334d36ce93ea02e6ab69b6
Nikita Atrashkevich <nkatrvld@gmail.com>
2019-04-21 23:52:15 +0300

    add to basket method add

--- Supreme/ViewModel/TestViewModel.cs
+++ Supreme/ViewModel/TestViewModel.cs
@@ -3,7 +3,12 @@ using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
 using Supreme.Core;
 using Supreme.Model;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Supreme.ViewModel
@@ -39,11 +44,63 @@ namespace Supreme.ViewModel
         #region Command
 
         private ICommand _AddToBasketCommand;
-        public ICommand AddToBasketCommand { get { return _AddToBasketCommand ?? (_AddToBasketCommand = new ActCommand(AddToBasket)); } }
+        public ICommand AddToBasketCommand { get { return _AddToBasketCommand ?? (_AddToBasketCommand = new ActCommand(GetDocument)); } }
 
         #endregion Command
 
         #region Method
+
+        async void fd()
+        {
+          var d = await SendAddToBasketRequestAsync(); 
+          
+        }
+
+        async Task<string> SendAddToBasketRequestAsync()
+        {
+            var dict = new Dictionary<string, string>();
+            string url = "https://www.supremenewyork.com/shop/304209/add";
+            dict.Add("urf8", "%E2%9C%93");
+            dict.Add("style", "25801");
+            dict.Add("size", "55214");
+            dict.Add("commit", "add to basket");
+            var xCSRFToken = "UEr1o5K8sG5V252eXER39Nq+/3275tn913tGY7X3NdVwzV7QtcndIn+SfTbonyn8y2nketCRVUiFwNIqK51EBA==";
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript");
+                client.DefaultRequestHeaders.Add("x-csrf-token", xCSRFToken);
+                client.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");                
+                HttpResponseMessage response = await client.PostAsync(url, new FormUrlEncodedContent(dict));
+                return response.Content.ReadAsStringAsync().Result; ;               
+            }
+        }
+
+
+        static async void MaindAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:6740");
+            //    var content = new FormUrlEncodedContent(new[]
+            //    {
+            //    new KeyValuePair<string, string>("", "login")
+            //});
+            //var content = new F
+
+                //var result = await client.PostAsync("/api/Membership/exists", content);
+                //string resultContent = await result.Content.ReadAsStringAsync();
+                //Console.WriteLine(resultContent);
+            }
+        }
+
+        private async void GetDocument()
+        {
+            var Document = DownloadHtml("https://www.supremenewyork.com/shop/jackets/f9d7nzc4j/ffh6c2zoj");
+            
+            var d = await SendAddToBasketRequestAsync();
+
+            int f = 0;
+        }
 
         public void AddToBasket()
         {
@@ -52,7 +109,9 @@ namespace Supreme.ViewModel
             var Items = new ObservableCollection<ArtList>();
 
             string Source = DownloadHtml("https://www.supremenewyork.com/shop/jackets/f9d7nzc4j/ffh6c2zoj");
+            //IConfiguration config = Configuration.Default.WithDefaultLoader();
             IConfiguration config = Configuration.Default.WithDefaultLoader();
+            //var config = Configuration.Default.WithJavaScript();
             IBrowsingContext context = BrowsingContext.New(config);
             HtmlParser Parser = new HtmlParser();
             IHtmlDocument Document = Parser.ParseDocument(Source);
@@ -70,6 +129,7 @@ namespace Supreme.ViewModel
             //var dd = context.Active.    QuerySelector<IHtmlFormElement>("dsf");
             Source = DownloadHtml("https://www.supremenewyork.com/shop/jackets/f9d7nzc4j/ffh6c2zoj");
             Document = Parser.ParseDocument(Source);
+            
             string check = "div[id=\"wrap\"] > div[id=\"container\"]  > div[id=\"details\"] > div[id=\"cctrl\"] > form[id=\"cart-remove\"]";
             var ss = Document.QuerySelector(check);
             //{
