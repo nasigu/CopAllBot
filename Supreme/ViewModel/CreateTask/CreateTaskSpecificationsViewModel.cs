3fa389bb863f0474728558738598c6a537438a0f
Nikita Atrashkevich <nkatrvld@gmail.com>
2019-05-30 00:00:13 +0300

    Combobox pre template, minor fixes

--- Supreme/ViewModel/CreateTask/CreateTaskSpecificationsViewModel.cs
+++ Supreme/ViewModel/CreateTask/CreateTaskSpecificationsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Supreme.Core;
 using Supreme.Model;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -14,6 +15,8 @@ namespace Supreme.ViewModel
 
         public ObservableCollection<Profile> Profiles { get; set; }
 
+        public ObservableCollection<Enums.Size> Sizes { get; set; }
+
         public CreateTaskSpecificationsViewModel()
         {
         }
@@ -26,21 +29,12 @@ namespace Supreme.ViewModel
                 "url"
             };
 
-            var profiles = new ObservableCollection<Profile>();
-            using (StreamReader r = new StreamReader("profiles.json"))
-            {
-                string json = r.ReadToEnd();
-                var tasks = JsonConvert.DeserializeObject<ListProfiles>(json);
-                foreach (var task in tasks.Profiles)
-                {
-                    profiles.Add(task);
-                }
-                Profiles = profiles;
-            }
             Parent = parent;
+            Profiles = GetProfiles();
+            Sizes = new ObservableCollection<Enums.Size>() { Enums.Size.Small, Enums.Size.Medium, Enums.Size.Large, Enums.Size.XLarge };
         }
 
-
+        
 
 
         public override void NextStep(CreateTaskViewModel CreateTaskVM)
@@ -49,6 +43,22 @@ namespace Supreme.ViewModel
 
             CreateTaskVM.Current = CreateTaskVM.finalize;
 
+        }
+
+        private ObservableCollection<Profile> GetProfiles()
+        {
+            var profiles = new ObservableCollection<Profile>();
+
+            using (StreamReader r = new StreamReader("profiles.json"))
+            {
+                string json = r.ReadToEnd();
+                var tasks = JsonConvert.DeserializeObject<ListProfiles>(json);
+                foreach (var task in tasks.Profiles)
+                {
+                    profiles.Add(task);
+                }
+                return profiles;
+            }
         }
     }
 }
